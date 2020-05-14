2e3cc0876df6e7e6161b26712e27255eb1f6e374
Alexander <zhlobale@gmail.com>
2020-05-15 00:51:18 +0300

    add mqtt_port option

--- mysgw/run.sh
+++ mysgw/run.sh
@@ -4,11 +4,12 @@ echo "MySensors Gateway"
 MYSGW_TYPE="$(jq --raw-output '.type' $CONFIG_PATH)"
 MYSGW_TRN="$(jq --raw-output '.transport' $CONFIG_PATH)"
 MQTT_SERVER="$(jq --raw-output '.mqtt_server' $CONFIG_PATH)"
+MQTT_PORT="$(jq --raw-output '.mqtt_port' $CONFIG_PATH)"
 MQTT_CLIENTID="$(jq --raw-output '.mqtt_clientid' $CONFIG_PATH)"
 MQTT_TOPIC_IN="$(jq --raw-output '.mqtt_topicin' $CONFIG_PATH)"
 MQTT_TOPIC_OUT="$(jq --raw-output '.mqtt_topicout' $CONFIG_PATH)"
 
-MQTT_OPTS="--my-mqtt-client-id=$MQTT_CLIENTID --my-controller-url-address=$MQTT_SERVER --my-mqtt-publish-topic-prefix=$MQTT_TOPIC_OUT --my-mqtt-subscribe-topic-prefix=$MQTT_TOPIC_IN"
+MQTT_OPTS="--my-mqtt-client-id=$MQTT_CLIENTID --my-controller-url-address=$MQTT_SERVER --my-port=$MQTT_PORT --my-mqtt-publish-topic-prefix=$MQTT_TOPIC_OUT --my-mqtt-subscribe-topic-prefix=$MQTT_TOPIC_IN"
 
 cd $APPDIR
 ./configure --spi-spidev-device=/dev/spidev0.0 --my-transport=$MYSGW_TRN --my-gateway=$MYSGW_TYPE $MQTT_OPTS
